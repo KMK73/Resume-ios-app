a586b20aec95e883658bbde324045efc08aefe7e
KMK73 <kmk73@miami.edu>
2016-02-11 18:38:02 -0500

    skills progress bar animation working

--- kelsey-resume/kelsey-resume/SkillsViewController.h
+++ kelsey-resume/kelsey-resume/SkillsViewController.h
@@ -8,6 +8,19 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SkillsViewController : UIViewController
+@interface SkillsViewController : UIViewController {
+    
+    NSTimer *progressTimer;
+    CGFloat HTMLprogressCount;
+    
+    IBOutlet UIProgressView *htmlProgress; 
+}
+
+- (IBAction)backToHomepage:(id)sender;
+
+//@property (weak, nonatomic) IBOutlet UIProgressView *htmlProgressBar;
+//timer for progress bars
+//@property (nonatomic, strong) NSTimer *progressTimer;
+
 
 @end
--- kelsey-resume/kelsey-resume/SkillsViewController.m
+++ kelsey-resume/kelsey-resume/SkillsViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+
+    [self setupProgress]; //run setup progress function
 }
 
 - (void)didReceiveMemoryWarning {
@@ -33,5 +35,43 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (void) setupProgress {
+    // Reset everything and start moving the progressbar
+    
+    //HTML & CSS SETUP FOR PROGRESS BAR ***************************
+    HTMLprogressCount = 0.0;
+    [htmlProgress setProgress:HTMLprogressCount];
+    progressTimer = [NSTimer scheduledTimerWithTimeInterval:0.1
+                                             target:self
+                                           selector:@selector(updateProgress)
+                                           userInfo:nil
+                                            repeats:YES];
+}
+
+- (void) updateProgress {
+    //HTML & CSS SETUP FOR PROGRESS BAR ***************************
+    HTMLprogressCount += 0.05; //increment by 5mil sec
+    NSLog(@"%f",HTMLprogressCount);
+    
+    [htmlProgress setProgress:HTMLprogressCount];
+    //HTML SET TO 85%
+    if(HTMLprogressCount >= 0.85) {
+        [progressTimer invalidate]; // stops timer, removes the method from the loop.
+    }
+    
+    
+}
+
+
+
+- (IBAction)backToHomepage:(id)sender {
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+}
+//
+//-(void) setProgress
+
 
 @end
