cb21f409fa41ecd419a2392284c2644b9393766b
KMK73 <kmk73@miami.edu>
2016-02-11 17:42:17 -0500

    added icons

---
+++ kelsey-resume/kelsey-resume/SkillsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SkillsViewController.h
+//  kelsey-resume
+//
+//  Created by Kelsey Kjeldsen on 2/11/16.
+//  Copyright © 2016 Kelsey Kjeldsen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SkillsViewController : UIViewController
+
+@end
---
+++ kelsey-resume/kelsey-resume/SkillsViewController.m
@@ -0,0 +1,37 @@
+//
+//  SkillsViewController.m
+//  kelsey-resume
+//
+//  Created by Kelsey Kjeldsen on 2/11/16.
+//  Copyright © 2016 Kelsey Kjeldsen. All rights reserved.
+//
+
+#import "SkillsViewController.h"
+
+@interface SkillsViewController ()
+
+@end
+
+@implementation SkillsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
